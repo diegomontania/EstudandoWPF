efe16cfeca2ed4c3f81600e4299b34cd9194f166
diego montania <diegoasmad1@gmail.com>
2020-12-07 22:36:17 -0300

    Alterações nas funções dos botões do form editar

--- ProjetoWPF_EventosDelegantesLambda/EdicaoAgencia.xaml.cs
+++ ProjetoWPF_EventosDelegantesLambda/EdicaoAgencia.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using ByteBank.Agencias.DAL;
 using System;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ProjetoWPF_EventosDelegantesLambda
 {
@@ -36,6 +38,7 @@ namespace ProjetoWPF_EventosDelegantesLambda
         {
             //cria um método anônimo, utilizando RoutedEventHandler que é utilizado em pequeno escopo
             //o resultado do método já ficará armazenado diretamente na variavel 'dialogResultTrue'
+            #region RoutedEventHandler
             //RoutedEventHandler dialogResultTrue = delegate (object o, RoutedEventArgs e)
             //{
             //    DialogResult = true;
@@ -45,6 +48,7 @@ namespace ProjetoWPF_EventosDelegantesLambda
             //{
             //    DialogResult = false;
             //};
+            #endregion
 
             //mesmo método anônimo que o anterior, porém usando expressão lambda
             RoutedEventHandler dialogResultTrue = (o, e) => DialogResult = true;
@@ -53,8 +57,46 @@ namespace ProjetoWPF_EventosDelegantesLambda
             var okEventHandler = dialogResultTrue + Fechar;
             var cancelarEventHandler = dialogResultFalse + Fechar;
 
+            //associando objetos do form aos seus metodos
             btnOk.Click += okEventHandler;
             btnCancelar.Click += cancelarEventHandler;
+
+            txtNome.TextChanged += ValidarCampoNulo;
+            txtDescricao.TextChanged += ValidarCampoNulo;
+            txtEndereco.TextChanged += ValidarCampoNulo;
+            txtNumero.TextChanged += ValidarCampoNulo;
+            txtTelefone.TextChanged += ValidarCampoNulo;
+        }
+
+        #region Metodo que retorna um delegate
+            //utilizacao
+            //txtNome.TextChanged += ConstruirDelegateValidacaoCampoNulo(txtNome);
+            //private TextChangedEventHandler ConstruirDelegateValidacaoCampoNulo(TextBox txt)
+            //{
+            //    //usando expressão lambda para o retorno
+            //    return (o, e) =>
+            //    {
+            //        var textoEstaVazio = String.IsNullOrEmpty(txtNome.Text);
+
+            //        if (textoEstaVazio)
+            //            txtNome.Background = new SolidColorBrush(Colors.OrangeRed);
+            //        else
+            //            txtNome.Background = new SolidColorBrush(Colors.White);
+            //    };
+            //}
+            #endregion
+
+        //metodo que retorna um delegate
+        //se o campo estiver vazio mude a cor para laranja
+        private void ValidarCampoNulo(object sender, EventArgs e)
+        {
+            var txt = sender as TextBox;
+            var textoEstaVazio = String.IsNullOrEmpty(txt.Text);
+
+            if (textoEstaVazio)
+                txt.Background = new SolidColorBrush(Colors.OrangeRed);
+            else
+                txt.Background = new SolidColorBrush(Colors.White);
         }
 
         private void Fechar(object sender, EventArgs e) =>
